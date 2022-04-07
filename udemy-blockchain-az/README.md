3edf935e70e6cbc6a639e8afe6f84b2139bdd0f3
Ayush singh <78909117+Ayushsingh07@users.noreply.github.com>
2022-04-07 17:15:39 +0530

    Add files via upload

---
+++ udemy-blockchain-az/README.md
@@ -0,0 +1,11 @@
+# Udemy Blockchain A-Z - nodejs Transcript
+
+[![Build Status](https://travis-ci.com/tl1/udemy-blockchain-az.svg?branch=master)](https://travis-ci.com/tl1/udemy-blockchain-az)
+
+This repo contains a JS transcript of the practical part of Udemy's
+"Blockchain A-Z" course. The original code is in Python using Flask.
+
+Course link: https://www.udemy.com/build-your-blockchain-az
+
+This is a personal project to learn a bit about Blockchains, Javascript
+and nodejs.
---
+++ udemy-blockchain-az/mempool.spec.js
@@ -0,0 +1,52 @@
+const mempool = require('./mempool');
+
+
+expect.extend({
+    toContainObject(received, argument) {  
+        const pass = this.equals(received, 
+          expect.arrayContaining([
+              expect.objectContaining(argument)
+          ])
+        )
+  
+        if (pass) {
+            return {
+                message: () => (`expected ${this.utils.printReceived(received)} not to contain object ${this.utils.printExpected(argument)}`),
+                pass: true
+            }
+        } else {
+            return {
+                message: () => (`expected ${this.utils.printReceived(received)} to contain object ${this.utils.printExpected(argument)}`),
+                pass: false
+            }
+        }
+    }
+});
+
+test('adds a transaction', () => {
+    const pool = mempool.addTx([], 'sender', 'receiver', 10);
+    expect(pool).toHaveLength(1);
+    expect(pool[0]).toMatchObject({
+        sender: 'sender',
+        receiver: 'receiver',
+        amount: 10
+    });
+    expect(pool[0].id).toBeDefined();
+});
+
+test('merges 2 pools', () => {
+    const pool1 = mempool.addTx([], 'paul', 'hannah', 102);
+    const pool2 = mempool.addTx(pool1, 'alice', 'bob', 20);
+    const merged = mempool.merge(pool1, pool2);
+    expect(merged).toHaveLength(2);
+    expect(merged).toContainObject({
+        sender: 'paul',
+        receiver: 'hannah',
+        amount: 102
+    });
+    expect(merged).toContainObject({
+        sender: 'alice',
+        receiver: 'bob',
+        amount: 20
+    });
+});
